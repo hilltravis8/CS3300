15cc5ff8b4ad79ddafd2556f99ee3ab1286dab8e
Travis Hill <hilltravis8@gmail.com>
2022-11-16 22:33:55 +0000

    Iteration 5 In Prog

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,10 @@ SimpleCov.start 'rails' do
   add_filter '/spec/' # for rspec
   add_filter '/app/helpers/application_helper.rb'
   add_filter '/app/helpers/projects_helper.rb'
+  add_filter '/app/channels/application_cable/channel.rb'
+  add_filter '/app/channels/application_cable/connection.rb	'
+  add_filter '/app/jobs/application_job.rb'
+  add_filter '/app/mailers/application_mailer.rb'
 end
 
 
